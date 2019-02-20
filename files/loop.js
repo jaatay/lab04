8998629176623138ef55821c726c35ccfae39678
Jesse Atay <jesse.atay@gmail.com>
2019-02-19 19:25:23 -0800

    tests for buffer loop finished

--- files/loop.js
+++ files/loop.js
@@ -2,4 +2,20 @@
 
 const testBuffer = Buffer.from('test');
 
-module.exports = testBuffer;
+//name array buffer - repl tested, repl approved
+const nameBuffer = new Buffer ([74, 101, 115, 115, 101, 44, 66, 111, 98, 44, 67, 108, 101, 116, 117, 115]);
+
+
+//buffer string test print method from demo code
+//because of file structure, in order to run use 'node ./files/loop.js' in CLI
+const customToString = buffer => {
+    let output = '';
+    for(let i = 0; i < buffer.length; i++) {
+      output += String.fromCharCode(buffer[i]);
+    }
+    return output;
+  };
+
+console.log(customToString(nameBuffer));
+
+module.exports = {testBuffer, nameBuffer};
